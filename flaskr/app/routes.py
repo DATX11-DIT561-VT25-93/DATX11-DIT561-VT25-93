716ab5ce4a164da13bc56ceff036971cb3c7362c
Erik Gälldin <erik.galldin@gmail.com>
2025-02-12 11:36:59 +0100

    Add sign_in and register routes

--- flaskr/app/routes.py
+++ flaskr/app/routes.py
@@ -13,3 +13,30 @@ def get_planets():
     response = supabase.table('test_planets').select("name").execute()
     return jsonify(response.data)
 
+@main.route('/sign_in', methods=['POST', 'GET'])
+def sign_in():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Needs login functionality
+        msg = 'Login attempt for ' + str(username)
+        return msg
+    return 'This is the login page'
+
+@main.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Needs registration functionality
+        msg = 'Registration attempt for user ' + str(username)
+        return msg
+    return 'This is the registration page'
+
+
+# For later
+@main.route('/frame', methods=['POST'])
+def handle_frame():
+    #frame = request...
+    return 'Frame handled'
+
