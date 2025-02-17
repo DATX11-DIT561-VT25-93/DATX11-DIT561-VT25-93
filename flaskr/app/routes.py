240ffbfa9f960fc8a31f09e6d551196cd2a5e74c
Erik Gälldin <erik.galldin@gmail.com>
2025-02-17 15:32:51 +0100

    Update routes.py

--- flaskr/app/routes.py
+++ flaskr/app/routes.py
@@ -6,23 +6,41 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('index.html')
 
-
 @main.route('/planets')
 def get_planets():
     supabase = current_app.supabase
     response = supabase.table('test_planets').select("description").execute()
     return jsonify(response.data)
 
-@main.route('/sign_in', methods=['POST', 'GET'])
-def sign_in():
+@main.route('/login', methods=['POST', 'GET'])
+def login():
     if request.method == 'POST':
         data = request.get_json()
+
         if not data:
             return jsonify({"error": "No JSON data received"}), 400
+        
         username = data.get('username')
         password = data.get('password')
+
+        if not username:
+            return jsonify({"error": "Username is required"}), 400
+        elif not password:
+            return jsonify({"error": "Password is required"}), 400
+        
+        # TODO: Check if username password combination exists in DB. Return appropriate messages for incorrect username and password, respectively.
+        # TODO: Add redirect for successfull sign in attempt
+
         return jsonify({"message": f"Sign in attempt for user {username}"})
-    return 'This is the sign in page'
+        #return render_template('login.html')
+
+    return 'This is the login page'
+    #return render_template('login.html')
+
+# TODO: Implement logout function below
+@main.route('/logout')
+def logout():
+    return 'This is the logout page'
 
 @main.route('/register', methods=['POST', 'GET'])
 def register():
@@ -34,23 +52,32 @@ def register():
         if not data:
             return jsonify({"error": "No JSON data received"}), 400
         
+        username = data.get('username')
+        password = data.get('password')
+
+        if not username:
+            return jsonify({"error": "Username is required"}), 400
+        elif not password:
+            return jsonify({"error": "Password is required"}), 400
+        
         new_user = {
             "username": data.get("username"),
             "password": data.get("password") 
+            # TODO: Hash the password before storing
         }
         
         # Insert new user into Supabase
         response = supabase.table('registered_users').insert(new_user).execute()
-
-        """ if response.get("error"):
-            return jsonify({"error": response.error.message}), 400 """
+        # TODO: Handle case where user already exists
 
         return jsonify({"message": "User added successfully", "data": response.data}), 201
-    
+        #return render_template('register.html')
+
     # Get registered users
     response = supabase.table('registered_users').select("username").execute()
 
     return jsonify(response.data)
+    #return render_template('register.html')
 
 
 # For later
