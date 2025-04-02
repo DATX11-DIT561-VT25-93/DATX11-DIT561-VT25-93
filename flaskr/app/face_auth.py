14c088ee3a74f5b9514a0e1003eb43b1d952dc0a
Mikael Motin <motin@chalmers.se>
2025-04-02 17:38:12 +0200

    quickfix: rearranged functions in face_auth

--- flaskr/app/face_auth.py
+++ flaskr/app/face_auth.py
@@ -125,49 +125,7 @@ def get_face_image():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-
-
-# Old code below, saved to show difference on meeting
-@face_auth_bp.route('/register-face-detection', methods=['POST', 'GET']) 
-def register_old():
-    if request.method == 'POST':
-        data = request.get_json()
-        
-        if 'image' not in data or 'email' not in data:
-            return jsonify({'error': 'Missing image or email'}), 400
-
-        try:
-            image_data = data['image'] # Webcam image in shape of base64 string        
-            email = data['email']
-            username = data['username']
-
-            print("Webcam frame received from " + str(email))
-            
-            face_data, new_image_data, image_rgb = detect_face(image_data) # Get array containing face data and image with marked faces in shape of base64 string
-           
-            if face_data is not None:
-                feature_vector = extract_feature(face_data, image_rgb, rec_model)
-                # Check for existing user
-                existing_user_check = check_existing_user(email)
-                
-                if existing_user_check[1] != 200:  
-                    print("Email already in use.")
-                    return existing_user_check  
-
-                else:
-                    print("user saved")
-                    save_user_to_db(email, feature_vector, username) # Adds new user to database
-                    session['user'] = email
-                    return jsonify({'message': 'Successful registration', 'new_image_data': new_image_data, "redirect": url_for('face_auth_bp.account')})
-      
-        except Exception as e:
-            return jsonify({"error": f"Invalid image data: {str(e)}"}), 400
-
-        return jsonify({'message': 'No face detected'})
-
-    return render_template('register-face-detection.html')
-
+    
 
 @face_auth_bp.route('/login-fr', methods=['POST', 'GET'])
 def login_fr():
@@ -216,10 +174,48 @@ def login_fr():
 
     return render_template('login.html')
 
-
-
 @face_auth_bp.route('/account')
 def account():
     if 'user' not in session:
         return redirect(url_for('face_auth_bp.login'))
     return render_template('account.html', username=session['user'])
+
+# Old code below, saved to show difference on meeting
+@face_auth_bp.route('/register-face-detection', methods=['POST', 'GET']) 
+def register_old():
+    if request.method == 'POST':
+        data = request.get_json()
+        
+        if 'image' not in data or 'email' not in data:
+            return jsonify({'error': 'Missing image or email'}), 400
+
+        try:
+            image_data = data['image'] # Webcam image in shape of base64 string        
+            email = data['email']
+            username = data['username']
+
+            print("Webcam frame received from " + str(email))
+            
+            face_data, new_image_data, image_rgb = detect_face(image_data) # Get array containing face data and image with marked faces in shape of base64 string
+           
+            if face_data is not None:
+                feature_vector = extract_feature(face_data, image_rgb, rec_model)
+                # Check for existing user
+                existing_user_check = check_existing_user(email)
+                
+                if existing_user_check[1] != 200:  
+                    print("Email already in use.")
+                    return existing_user_check  
+
+                else:
+                    print("user saved")
+                    save_user_to_db(email, feature_vector, username) # Adds new user to database
+                    session['user'] = email
+                    return jsonify({'message': 'Successful registration', 'new_image_data': new_image_data, "redirect": url_for('face_auth_bp.account')})
+      
+        except Exception as e:
+            return jsonify({"error": f"Invalid image data: {str(e)}"}), 400
+
+        return jsonify({'message': 'No face detected'})
+
+    return render_template('register-face-detection.html')
